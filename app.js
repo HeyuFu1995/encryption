027e6e68ed2c26cc2de9fa8fa31bb5ac9abefe62
Heyu Fu <Herry Fu>
2024-01-05 17:17:03 +1100

    Level 6: OAuth 2.0 With Google

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import bcrypt from 'bcrypt';
 import passport from 'passport';
 import passportLocalMongoose from "passport-local-mongoose";
 import session from "express-session";
+import dotenv from 'dotenv';
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import findOrCreate from 'mongoose-findorcreate';
+
+dotenv.config();
 
 
 const app = express();
@@ -17,9 +21,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('trust proxy', 1); // trust first proxy
 app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false,
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -28,17 +32,45 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 // userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, {
+            id: user.id,
+            username: user.username,
+            picture: user.picture
+        });
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+},
+    (accessToken, refreshToken, profile, cb) => {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -48,9 +80,22 @@ app.get("/login", (req, res) => {
     res.render("login");
 });
 
+app.get("/auth/google", (req, res) => {
+    passport.authenticate("google", { scope: ["profile"] })(req, res, () => {
+        res.redirect("/secrets");
+    });
+});
+
 app.get("/register", (req, res) => {
     res.render("register");
 });
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    (req, res) => {
+        // Successful authentication, redirect home.
+        res.redirect('/secrets');
+    });
 
 app.post("/login", async (req, res) => {
     const email = req.body.username;
@@ -62,7 +107,7 @@ app.post("/login", async (req, res) => {
     });
 
     req.login(user, (err) => {
-        if(err) {
+        if (err) {
             console.log(err);
             res.redirect("/login");
         } else {
@@ -76,8 +121,8 @@ app.post("/login", async (req, res) => {
 app.post("/register", (req, res) => {
     const email = req.body.username;
     const password = req.body.password;
-    User.register({username: email}, password, (err, user) => {
-        if(err) {
+    User.register({ username: email }, password, (err, user) => {
+        if (err) {
             console.log(err);
             res.redirect("/register");
         } else {
@@ -89,7 +134,7 @@ app.post("/register", (req, res) => {
 });
 
 app.get("/secrets", (req, res) => {
-    if(req.isAuthenticated()) {
+    if (req.isAuthenticated()) {
         res.render("secrets");
     } else {
         res.redirect("/login");
